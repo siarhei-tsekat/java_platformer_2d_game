30a7331fe6886510747c7dafe6df8e50f58efb7b
tsekot sergey <tsekot.sergey@gmail.com>
2024-04-19 06:54:07 +0200

    5

---
+++ src/main/java/com/me/levels/LevelManager.java
@@ -0,0 +1,47 @@
+package com.me.levels;
+
+import com.me.Game;
+import com.me.utils.LoadSave;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class LevelManager {
+
+    private Game game;
+    private BufferedImage[] levelSprite;
+    private Level levelOne;
+
+    public LevelManager(Game game) {
+        this.game = game;
+        importOutsideSprites();
+        levelOne = new Level(LoadSave.getLevelData());
+    }
+
+    public void draw(Graphics graphics) {
+        for (int j = 0; j < Game.TILES_IN_HEIGHT; j++) {
+            for (int i = 0; i < Game.TILES_IN_WIDTH; i++) {
+                int index = levelOne.getSpriteIndex(i, j);
+                graphics.drawImage(levelSprite[index], i * Game.TILES_SIZE, j * Game.TILES_SIZE, Game.TILES_SIZE, Game.TILES_SIZE, null);
+            }
+        }
+        graphics.drawImage(levelSprite[2], 0, 0, null);
+    }
+
+    public void update() {
+
+    }
+
+    private void importOutsideSprites() {
+        BufferedImage img = LoadSave.getSpriteAtlas(LoadSave.LEVEL_ATLAS);
+
+        levelSprite = new BufferedImage[48];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 12; j++) {
+                int index = i * 12 + j;
+                levelSprite[index] = img.getSubimage(j * 32, i * 32, 32, 32);
+            }
+        }
+    }
+}
